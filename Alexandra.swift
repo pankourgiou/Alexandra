feefc115abcd086eddcf34a6ca5df6aa044570d7
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 08:02:37 +0200

    Create Alexandra.swift

---
+++ Alexandra.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+let  AlexandraIsAGoodPerson = true // Boolean variable
+let  AlexandraIsASmartPerson = true // Another Boolean variable
+
+// Simple conditional check
+if AlexandraIsAGoodPerson {
+    print("She is a good person")
+} else {
+    print("She is smart")
+}
+import Foundation
+
+let  AlexandraHasAGoodBody = true // Boolean variable
+let  AlexandraIsSerious = true // Another Boolean variable
+
+// Simple conditional check
+if AlexandraIsAGoodPerson {
+    print("She has a good body")
+} else {
+    print("She is serious")
+}
+import Foundation
+
+let  AlexandraHasAnEyeThingButstillBeautifulEyes = true // Boolean variable
+let  alexandraIsSerious = true // Another Boolean variable
+
+// Simple conditional check
+if AlexandraHasAnEyeThingButstillBeautifulEyes {
+    print("We should check them")
+} else {
+    print("you are a good person  Alexandra we will check your eyes")
+}
+import Foundation
+
+let  Greetings_to_Konstantina = true // Boolean variable
+let  greetings_to_Konstantina = true // Another Boolean variable
+
+// Simple conditional check
+if Greetings_to_Konstantina {
+    print("I hope Mrs.Marinella gets better, Konstantina likes her I remember")
+} else {
+    print("it is a bit cold but a good day")
+}
+//Some action...
+import Foundation
+
+let  PAOK_lost = true // Boolean variable
+let  PAO_won = true // Another Boolean variable
+
+// Simple conditional check
+if PAOK_lost {
+    print("we need to exercise!")
+} else {
+    print("we need to exercise!")
+}
+
